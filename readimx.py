a90761ae9bc9d1137a4bb6cf5eb62bd7f4adaf65
elenamanjavacas <54870952+elenamanjavacas@users.noreply.github.com>
2020-05-07 20:49:17 -1000

    Update readimx.py

--- readimx.py
+++ readimx.py
@@ -5,11 +5,12 @@
 
 import numpy as np
 import os.path
-
+import matplotlib.pyplot as plt
 
 def readimx(filename):
     path2imx='/kroot/rel/ao/qfix/data/ControlParms/SystemMatrix/'
     
+    Hinv = np.zeros((349,608))
     tmp0 = filename
     tmp = os.path.isfile(tmp0)
     print('File found? =',tmp)
@@ -22,13 +23,19 @@ def readimx(filename):
             print('File '+filename+' not found, returning')
         else:
             fname = tmp0
-            H =  np.fromfile(fname, dtype='f', offset=0).reshape((349,608))
+            H =  np.fromfile(fname, dtype='f', offset=1)
+            H = np.insert(H,0,0)
+            H = H.reshape((349,608))
+            return H
             print(H,np.shape(H))
             np.savetxt(fname, H)
             print('File '+ 'new_'+fname +' written in path '+path2imx)
     else: 
         fname = tmp0
-        H =  np.fromfile(fname, dtype='f', offset=0).reshape((349,608))
-        print(H,np.shape(H))
+        H = np.fromfile(fname, dtype='f', offset=1)
+        H = np.insert(H,0,0)
+        H = H.reshape((349,608))
+        return H
+        plt.plot(H)
         np.savetxt('new_'+fname, H)
         print('File '+ 'new_'+fname +' written')
