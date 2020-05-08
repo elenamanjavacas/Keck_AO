c48aee8b91e6e55d8383906bccfb613cfcd0a2e5
elenamanjavacas <54870952+elenamanjavacas@users.noreply.github.com>
2020-05-07 20:47:41 -1000

    Update readcog.py

--- readcog.py
+++ readcog.py
@@ -6,31 +6,34 @@
 
 import numpy as np
 import os.path
+import matplotlib.pyplot as plt
 
 def readcog(filename):
     offsets = np.zeros(608)
     cdcog='/local/kroot/rel/ao/qfix/data/ControlParms/CentOrigin/'
-
+    
     tmp0 = filename
     tmp = os.path.isfile(tmp0)
     print('File found? =',tmp)
-
+    
     if tmp == False:
         tmp0 = cdcog+filename
         tmp = os.path.isfile(tmp0)
         print('File found in path '+cdcog+'? = ' ,tmp)
-
+        
         if tmp == False:
             print('File '+filename+' not found, returning')
-
+            
         else:
             fname = tmp0
-            offsets = np.loadtxt(fname)
-            np.savetxt(fname, offsets)
+            offsets = np.fromfile(fname, dtype='f', offset=1)
+            np.savetxt('new_'+fname, offsets)
             print('File written in path '+ cdcog)
-
-    else:
+            return offsets
+    else: 
         fname = tmp0
-        offsets = np.loadtxt(fname)
-        np.savetxt(fname, offsets)
+        offsets = np.fromfile(fname, dtype='f', offset=1)
+        plt.plot(offsets)
+        np.savetxt('new_'+fname, offsets)
         print('File '+ filename +' written')
+        return offsets
