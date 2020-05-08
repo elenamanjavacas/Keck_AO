4f579efdb5d6458a247405af5029ad1452f0d705
elenamanjavacas <54870952+elenamanjavacas@users.noreply.github.com>
2020-05-07 20:52:04 -1000

    Update readph.py

--- readph.py
+++ readph.py
@@ -5,15 +5,16 @@
 
 import numpy as np
 import os.path
+import matplotlib.pyplot as plt
 
 def readph(filename):
     cdph = '/kroot/rel/ao/qfix/data/'
     phase = np.zeros(349)
-
+    
     tmp0 = filename
     tmp = os.path.isfile(tmp0)
     print('File found? =',tmp)
-
+    
     if tmp == False:
         tmp0 = cdph+filename
         tmp = os.path.isfile(tmp0)
@@ -22,12 +23,18 @@ def readph(filename):
             print('File '+filename+' not found, returning')
         else:
             fname = tmp0
-            phase = np.loadtxt(fname)
-            np.savetxt(fname, phase)
+            phase = open(fname,'r').read()
+            phase = np.array(phase.replace("\n", "").split()).astype(float)
+            plt.plot(phase)
             print('File written in path '+ cdph)
-
-    else:
+            return phase
+    else: 
         fname = tmp0
-        phase = np.loadtxt(fname)
-        np.savetxt(fname, phase)
+        phase = open(fname,'r').read()
+        phase = np.array(phase.replace("\n", "").split()).astype(float)
+        
+        #print(phase)
+        plt.plot(phase)
+        #np.savetxt('new'+fname, phase)
         print('File '+ filename +' written')
+        return phase
