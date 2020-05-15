2874394f0bcd7fcbbf8df2de61492a0ebb2d4a0b
elenamanjavacas <54870952+elenamanjavacas@users.noreply.github.com>
2020-05-15 11:08:57 -1000

    Create writedm.py

---
+++ writedm.py
@@ -0,0 +1,15 @@
+# To run writedm include the file name as first input, and the path to the dm file as second input. 
+# If the dm file is in the current directory, then include write ''.
+
+def writedm(filename, path2dm):
+    if path2dm == 'path':
+        cddm='/kroot/rel/ao/qfix/data/ControlParms/MirrorOrigin/'
+        file = np.fromfile(cddm+filename, dtype='f', offset=1)
+        plt.plot(file)
+        np.savetxt('new_'+filename, file)
+        print('File '+ 'new_'+filename +' written')
+    else:
+        file = np.fromfile(filename, dtype='f', offset=1)
+        plt.plot(file)
+        np.savetxt('new_'+filename, file)
+        print('File '+ 'new_'+filename +' written')
